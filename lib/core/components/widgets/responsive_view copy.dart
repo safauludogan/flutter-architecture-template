67c3181b7e60bb80ff33665f784ba9761938739d
Safa <suludogan95@gmail.com>
2023-03-03 13:18:30 +0300

    ResponsiveView eklendi

---
+++ lib/core/components/widgets/responsive_view copy.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture_template/core/extension/context_extension.dart';
+
+class ResponsiveView extends StatelessWidget {
+  const ResponsiveView({super.key, required this.mobile, required this.web});
+  final Widget mobile;
+  final Widget web;
+
+  static bool isSmallMuchScreen(BuildContext context) {
+    return context.width < 450;
+  }
+
+  static bool isSmallScreen(BuildContext context) {
+    return context.width < 800;
+  }
+
+  static bool isLargeScreen(BuildContext context) {
+    return context.width > 1200;
+  }
+
+  static bool isMediumScreen(BuildContext context) {
+    return context.width >= 800 && context.width <= 1200;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (kIsWeb) {
+          return web;
+        }
+        return mobile;
+      },
+    );
+  }
+}
